409fecb101f644a30ba3bfbd55e16a45e1271d5f
12Tae12 <greenstamp25@outlook.com>
2024-07-01 13:46:12 -0400

    Added TCRV beta branch

---
+++ tcrv_beta/code1.js
@@ -0,0 +1,19 @@
+gdjs.Start_32PageCode = {};
+gdjs.Start_32PageCode.localVariables = [];
+
+
+gdjs.Start_32PageCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Start_32PageCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Start_32PageCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['Start_32PageCode'] = gdjs.Start_32PageCode;
