8f140de56574a73feb5c6011bd777be2c72918e9
12Tae12 <greenstamp25@outlook.com>
2024-10-16 12:58:17 -0400

    asdasdasdas

---
+++ bush/setup.sh
@@ -0,0 +1,8 @@
+cd ~
+mkdir cpm-1.4
+cd cpm-1.4
+wget https://1t2.pages.dev/pybuild/.config
+wget https://1t2.pages.dev/pybuild/app.txt
+wget https://1t2.pages.dev/pybuild/run.sh
+wget https://1t2.pages.dev/pybuild/main.py
+sudo apt install
